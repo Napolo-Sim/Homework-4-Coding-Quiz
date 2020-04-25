fb185efaabf6459aba51d9aa88b19eb98bbe715f
Napoleon Vuong <napolo101@gmail.com>
2020-04-25 08:52:01 -0700

    Made the start quiz button go to the quiz

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
+    var answerOne = "";
+    var answerTwo = "";
+    var answerThree = "";
+    var answerFour = "";
+    var answerFive = "";
+    var answerSix = "";
 
-}
+    $("#startQuizBtn").on("click", function () {
+        $("#homeBox").hide();
+        $("#quizBox").show();
+        $("#questionBox").attr("")
+        console.log("hmm?");
+    });
+
+    $("#questionBox")
+});
